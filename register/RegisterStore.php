696ab9f5b7a1f14e04f00674307b96389b0cdb79
MehranHamidian <www.mehranhamidian@gmail.com>
2024-11-13 12:13:06 +0330

    use the functions to validate username, firstname and lastname

--- register/RegisterStore.php
+++ register/RegisterStore.php
@@ -1,6 +1,8 @@
 <?php
-    $pdo = require_once __DIR__. "/../connection.php";
-    require_once __DIR__. "/../inc/functions/DB/ExistInDB.php";
+    $pdo = require_once __DIR__. "/../connection.php"; //for connection to database
+    require_once __DIR__. "/../inc/functions/DB/ExistInDB.php"; //for check value in database
+    require_once __DIR__. "/../inc/functions/validate/validate_username.php"; //for validate username
+    require_once __DIR__. "/../inc/functions/validate/validate_name.php"; //for validate names
 ?>
 <?php
 
@@ -15,60 +17,34 @@
     //get username from register form
     $username = strtolower(trim(htmlspecialchars(filter_input(INPUT_POST, "username"))));
     $inputs["username"] = $username;
-    //check for if user name empty. if the username field be empty, send an error message to the user.
-    //In the 'register.php' page, we check if the count of errors is greater than 0. if so, we include the form page.
-    //check "register.php" for this.
-    if(empty($username)){
-        $errors["username"] = "username is required";
-    }else{
-        //useranme should only contain alphabetic character and "_", we check it by preg_match() functoin.
-        //We regenerate the registration form for the user if any error occurs.
-        if(preg_match('/[^a-z_]/', $username)){
-            $errors['username'] = "user name should only contain alphabetic character and '_'";
-        }elseif(strlen($username) < 3 ){//this for check length of username if less than 3, we have an error
-            $errors["username"] = "minimum character for username is 3";
-        }elseif(strlen($username) > 64){//and also morethan 64
-            $errors["username"] = "maximum character for username is 64";
-        }
+    
+    //validate user name by validate_usernmae function
+    if(validate_username($username) !== true){
+        $errors["username"] = validate_username($username);
     }
     //check for if username already exist?
     if(Exist("user", "username", $inputs["username"])){
         $errors["username"] = "the username already exist!";
     }
 
-    //validate first name
-    $FirstName = trim(htmlspecialchars(filter_input(INPUT_POST,"FirstName")));
+    //get first name
+    $FirstName = htmlspecialchars(filter_input(INPUT_POST,"FirstName"));
     $inputs["FirstName"] = $FirstName;
-    //Now, check if it is empty??
-    if(empty($FirstName)){
-        $errors["FirstName"] = "First name field is required.";
-    }else{
-        //First name field should contain only alphabetic characters.
-        if(preg_match('/[^a-zA-Z]/', $FirstName)){
-            $errors["FirstName"] = "First name field should contain only alphabetic characters.";
-        }elseif(strlen($FirstName) < 2){
-            $errors["FirstName"] = "minimum character for first name is 2";
-        }elseif(strlen($FirstName) > 64){
-            $errors["FirstName"] = "maximun character for first name is 64";
-        }
+    //validate first name by validate_name function
+    if(validate_name($FirstName) !== true){
+        $errors["FirstName"] = validate_name($FirstName);
     }
 
-    //validate last name
-    $LastName = trim(htmlspecialchars(filter_input(INPUT_POST, "LastName")));
+    //get the last name
+    $LastName = htmlspecialchars(filter_input(INPUT_POST, "LastName"));
     $inputs["LastName"] = $LastName;
-    //the LastName field is not requierd, but if user want insert it's last name check for not be less than 2 character
-    if(!empty($LastName)){
-        if(strlen($LastName)< 2){
-            $errors["LastName"] = "minimum charcter for last name is 2";
-        }elseif(preg_match('/[^a-zA-Z]/', $LastName)){
-            $errors["LastName"] = "First name field should contain only alphabetic characters.";
-        }elseif(strlen($LastName)> 64){
-            $errors["LastName"] = "maximum character for last name is 64";
-        }
+    //validate it
+    if(validate_name($LastName) !== true){
+        $errors["LastName"] = validate_name($LastName);
     }
 
     //validate email
-    $email = filter_input(INPUT_POST, "email"); //get email
+    $email = htmlspecialchars(filter_input(INPUT_POST, "email")); //get email
     $inputs["email"] = $email;
     if(empty($email)){ //check if email empty
         $errors["email"] = "email field is required";
